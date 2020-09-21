6c0be861be85eb34b272ddaab0d34aa446d1914c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-09-21 21:09:39 +1000

    move [publish] stripping to the correct file

--- .github/actions/build-tag.sh
+++ .github/actions/build-tag.sh
@@ -5,9 +5,6 @@ sed -i -e "s|#PARALLEL_MAKE=.*|PARALLEL_MAKE=4|" \
     -e "s|PUBLISH_API_KEY=.*|PUBLISH_API_KEY=$API_KEY|" \
     local.mk
 
-# GITHUB_REF="refs/tags/dnscrypt-proxy[publish]-2.0.42"
-GITHUB_REF="${GITHUB_REF//\[publish\]}" # strip [publish]
-
 # PACKAGE=$(echo "refs/tags/dnscrypt-proxy-2.0.42" | grep -oE "([0-9a-zA-Z]*-)*")
 GH_PACKAGE=$(echo "$GITHUB_REF" | grep -oE "([0-9a-zA-Z]*-)*")
 GH_PACKAGE="${GH_PACKAGE:0:-1}"
